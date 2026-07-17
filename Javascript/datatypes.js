5a2fe8e0336d544dd543953827dfde56a8d35f9a
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-17 14:35:50 +0530

    Datatypes

---
+++ Javascript/datatypes.js
@@ -0,0 +1,39 @@
+let age = 25;
+console.log(age);
+console.log(typeof age);
+
+// string
+
+let name = "Shivam";
+console.log(name);
+console.log(typeof (name));
+
+// boolean
+
+let Student = true;
+console.log(Student);
+console.log(typeof (Student));
+
+//undefined
+
+let x;  
+console.log(x);
+console.log(typeof (x));
+
+//null
+
+let y = null;
+console.log(y);
+console.log(typeof (y));
+
+//symbol
+
+let id = Symbol("id");
+console.log(id);
+console.log(typeof (id));
+
+// bigint
+
+let num = 1234567890123456789012345678901234567890n;
+console.log(num);
+console.log(typeof (num));
