9c54e9c9494b244a2cf8523ba065c2b1b6b4a3a9
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-29 09:40:46 +0530

    Day-1

---
+++ Javascript/DAY-1/script.js
@@ -0,0 +1,47 @@
+console.log("Hello, World");
+
+// Variables
+
+// var
+var name= "Shivam"
+console.log(name);
+
+name= "Rahul"
+console.log(name);
+
+var name= "Vansh"
+console.log(name);
+
+//let
+
+let a= 25;
+
+console.log(a);
+
+a= 10;
+console.log(a);
+
+//const
+
+const b= "India"
+console.log(b);
+
+
+// How to update const using object
+
+const s= {
+    name: "Rahul",
+    age: 18
+}
+
+s.name="Satyam"
+console.log(s);
+
+
+// How to update const using arr
+
+const fruit=["apple", "banana"];
+
+fruit.push("mango");
+fruit[0]="abc";
+console.log(fruit);
