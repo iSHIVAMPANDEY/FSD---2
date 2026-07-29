53d77d9c11af47497847740bbc1a55587559e8c3
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-29 09:51:16 +0530

    .

---
+++ Javascript/DAY-1/script2.js
@@ -0,0 +1,16 @@
+// Type of Error
+/*
+console.log("Hello"  // syntax error
+
+console.log(name);  // reference error
+
+const num=10;
+nums();         // type error
+
+const num= 10;  // type error
+num=20;
+
+const arr = new Array(-5);  // range error
+
+decodeURL("%");
+*/
