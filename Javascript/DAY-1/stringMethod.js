c2fccdbb5764a2086122d25509427734e7c5a6cd
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-29 10:19:52 +0530

    Day-1

---
+++ Javascript/DAY-1/stringMethod.js
@@ -0,0 +1,21 @@
+let text= "JavaScript programming";
+
+console.log(text.length); // length of string
+
+console.log(text.toUpperCase()); // convert to uppercase
+
+console.log(text.toLowerCase()); // convert to lowercase
+
+console.log(text.includes("Hello")); //  whether the text included or not
+
+console.log(text.startsWith("Ja")); //
+
+console.log(text.endsWith("ing")); //
+
+console.log(text.slice(0,22)); // print the text from an index to an particular index
+
+console.log(text.substring(0,10));
+
+console.log(text.split(" ")); // split the string into an array of substrings
+
+
---
+++ Javascript/DAY-1/templateLiterals.js
@@ -0,0 +1,9 @@
+// Template Literals
+
+let name= "Shivam";
+let age= 25;
+let city= "Delhi";
+
+console.log("name is: "+name);      // Used previously
+
+console.log(`name is: ${name}`);        // Recently update in JS due to REACT
