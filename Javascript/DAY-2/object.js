4091ec72648e4bb63dabb9159ed486d184b58513
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-30 12:09:07 +0530

    .

---
+++ Javascript/DAY-2/object.js
@@ -0,0 +1,19 @@
+// Math Object
+
+console.log(Math.PI);
+
+console.log(Math.sqrt(4));
+
+console.log(Math.pow(2,3));
+
+console.log(Math.max(34,69,55,4));
+
+console.log(Math.min(34,69,55,4));
+
+console.log(Math.floor(10.99));
+
+console.log(Math.ceil(10.01)); 
+
+console.log(Math.round(10.01)); 
+
+console.log(Math.floor(Math.random()*100+1)); // random picks random no thats why ouput is diff everytime
