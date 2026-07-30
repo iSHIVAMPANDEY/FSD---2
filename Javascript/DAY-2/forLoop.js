21562eb06b8d226d2f7f17dd0a1199303a46dfd3
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-30 12:07:39 +0530

    .

---
+++ Javascript/DAY-2/forLoop.js
@@ -0,0 +1,19 @@
+// let student={
+// name: "Shivam",
+// age: 18,
+// }
+
+// // Two types of for - for in and for of
+
+// //for in
+// for(let key in student){
+// console.log(key+ " : "+student[key]);
+// }
+
+let f= ["apple","banana","mango"]
+
+for(const fruit of f){
+    console.log(fruit);
+}
+
+
