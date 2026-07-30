939eefbb237424d82db851c2cb68c722a46b8dc6
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-30 12:08:33 +0530

    .

---
+++ Javascript/DAY-2/numberMethod.js
@@ -0,0 +1,9 @@
+let num= 123.45678;
+
+console.log(num.toFixed(2)); //num which is to be fixed 
+
+console.log(num.toPrecision(5)); // till what digit do you want the ouput
+
+console.log(Number.isInteger(num));    //check whether it is int or not
+
+console.log(Number.parseInt(num));  //provide only int value
