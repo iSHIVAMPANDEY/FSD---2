71f70175f4f225fe862a8f99bdd2505f0507544b
iSHIVAMPANDEY <i.shivampandeyy@gmail.com>
2026-07-30 12:05:51 +0530

    DAY-2

---
+++ Javascript/DAY-2/arrayAdvance.js
@@ -0,0 +1,20 @@
+let num = [10,20,30,40,50]
+
+num.push(60);
+console.log(num);
+
+console.log(num.unshift(5)); // shift the 5 index no to 6 (which mean 6th no transfer to 7th)
+
+console.log(num.pop());
+
+console.log(num.shift());
+
+console.log(num.indexOf(30));
+
+console.log(num.includes(40));
+
+console.log(num.reverse());
+
+console.log(num.sort());
+
+console.log(num.join());
